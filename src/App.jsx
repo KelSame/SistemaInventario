f7fc48c33faccd7b4890613ac1a2b2b8be68c17e
Kelvin Code <kelvin@kelcoding.com>
2026-04-20 20:22:30 -0500

    Modificacionsrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./Routes/AppRoutes";
 import { AuthProvider } from "./Context/AuthContext";
 
 import './App.css'
